f39916bd822916be78306092c3085a3aaed1b10f
Spencer Schultz <srs27@students.calvin.edu>
2016-01-13 10:24:46 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,15 @@ public class Main {
 		GameMap gameMap = new GameMap("MapOpenRoom20x20y.txt");
 		Scanner in = new Scanner(System.in);
 		char input = 'r';
-		int xPosition = 2;
-		int yPosition = 2;
+		int xPosition = 1;
+		int yPosition = 1;
+		
 
 		//movement screen
 		boolean isRunning = true;
 		while (isRunning == true) {
 			System.out.println("You are standing on a " + gameMap.getTile(xPosition, yPosition).getDescription() + ".\n");
-			System.out.println("Please press the a, w, d, or s keys to move around the map. Pressing q will quit. \n");
+			System.out.println("Please press the a, w, d, or s keys to move around the map. Pressing q will quit.");
 			
 			try {
 				input = (char)System.in.read();
@@ -30,26 +31,30 @@ public class Main {
 			}
 			in.nextLine();
 			if (input == 'a') {
-				if (gameMap.getTile(xPosition, yPosition).canBeMovedTo()) {
+				if (gameMap.getTile((xPosition - 1), yPosition).canBeMovedTo()) {
 					xPosition--;
+					System.out.println("Moving left...");
 				} else {
 					System.out.println("Cannot move there!\n\n\n");
 				}
 			} else if (input == 'w') {
-				if (gameMap.getTile(xPosition, yPosition).canBeMovedTo()) {
-					yPosition++;
+				if (gameMap.getTile(xPosition, (yPosition - 1)).canBeMovedTo()) {
+					yPosition--;
+					System.out.println("Moving forwards...");
 				} else {
 					System.out.println("Cannot move there!\n\n\n");
 				}
 			} else if (input == 's') {
-				if (gameMap.getTile(xPosition, yPosition).canBeMovedTo()) {
-					yPosition--;
+				if (gameMap.getTile(xPosition, (yPosition + 1)).canBeMovedTo()) {
+					yPosition++;
+					System.out.println("Moving backwards...");
 				} else {
 					System.out.println("Cannot move there!\n\n\n");
 				}
 			} else if (input == 'd') {
-				if (gameMap.getTile(xPosition, yPosition).canBeMovedTo()) {
+				if (gameMap.getTile((xPosition + 1), yPosition).canBeMovedTo()) {
 					xPosition++;
+					System.out.println("Moving right...");
 				} else {
 					System.out.println("Cannot move there!\n\n\n");
 				}
@@ -63,6 +68,10 @@ public class Main {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			//clear console
+			System.out.print("\n\n\n\n\n");
 		}
+		in.close();
 	}
+	
 }
